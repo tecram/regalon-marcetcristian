62ebffcdf6d7ae56481764f00b877644fd219e21
tecram <marcet_cristian@yahoo.com.ar>
2022-07-04 19:30:28 -0300

    Remove console log

--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -1,7 +1,6 @@
 import './ItemListContainer.scss'
 
 const ItemListContainer = (props) => {
-  console.log(props.greeting)
   return ( <section className="item-list-container">{props.greeting}</section> );
 }
  
