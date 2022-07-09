a85d73a151771e45e21d736aa1c6e100e3809bd9
tecram <marcet_cristian@yahoo.com.ar>
2022-07-09 13:43:27 -0300

    Partial commit

--- src/components/Item/Item.jsx
+++ src/components/Item/Item.jsx
@@ -6,7 +6,6 @@ const Item = (item) => {
       <div className="product-image"><img src="" alt="" /></div>
       <p className="product-name"></p>
       <p className="product-sku"></p>
-      <p className="product-description"></p>
       <p className="product-price"></p>
     </div>
    );
