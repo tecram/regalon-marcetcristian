61212124ee52e15736b6f25edb1cd3ea669b7aaa
tecram <marcet_cristian@yahoo.com.ar>
2022-07-09 13:35:16 -0300

    Partial commit

---
+++ src/components/Item/Item.jsx
@@ -0,0 +1,15 @@
+
+
+const Item = (item) => {
+  return ( 
+    <div className="product-card">
+      <div className="product-image"><img src="" alt="" /></div>
+      <p className="product-name"></p>
+      <p className="product-sku"></p>
+      <p className="product-description"></p>
+      <p className="product-price"></p>
+    </div>
+   );
+}
+ 
+export default Item;
--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -1,13 +1,15 @@
 import './ItemListContainer.scss'
-import ItemCount from '../ItemCount/ItemCount';
+/* import ItemCount from '../ItemCount/ItemCount'; */
+import ItemList from '../ItemList/ItemList';
 
 const ItemListContainer = (props) => {
   return ( 
     <section className="item-list-container">
-      <p>{props.greeting}</p>
+      {/* <p>{props.greeting}</p>
       <section className='product-cards'>
         <ItemCount stock="5" initial="1" />
-      </section>
+      </section> */}
+      <ItemList />
     </section> 
   )
 }
