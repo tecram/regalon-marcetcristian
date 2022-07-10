7455d81c6cd5cab0bca6cbf3068d92ff257f4f6e
tecram <marcet_cristian@yahoo.com.ar>
2022-07-10 17:29:02 -0300

    Correccion de metodo onAdd

--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -2,11 +2,15 @@ import './ItemListContainer.scss'
 import ItemCount from '../ItemCount/ItemCount';
 
 const ItemListContainer = (props) => {
+  const onAdd = (count) => {
+    console.log(count)
+  }
+
   return ( 
     <section className="item-list-container">
       <p>{props.greeting}</p>
       <section className='product-cards'>
-        <ItemCount stock="5" initial="1" />
+        <ItemCount stock="5" initial="1" onAdd={onAdd} />
       </section>
     </section> 
   )
