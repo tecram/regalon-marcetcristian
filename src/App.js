b4b34e2e98f59e814b7df55a4998ff1dd7c7d7d5
tecram <marcet_cristian@yahoo.com.ar>
2022-07-13 19:53:17 -0300

    Partial commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import NavBar from './components/NavBar/NavBar'
-import ItemListContainer from './components/ItemListContainer/ItemListContainer';
+/* import ItemListContainer from './components/ItemListContainer/ItemListContainer' */
+import ItemDetailContainer from './components/ItemDetailContainer/ItemDetailContainer';
 
 function App() {
   return <>
     <NavBar />
-    <ItemListContainer greeting="Bienvenido a la tienda Regalon" />
+    {/* <ItemListContainer greeting="Bienvenido a la tienda Regalon" /> */}
+    <ItemDetailContainer />
   </>
 }
 
---
+++ src/components/ItemDetail/ItemDetail.jsx
@@ -0,0 +1,32 @@
+import './ItemDetail.scss'
+
+const ItemDetail = ({ product, loading }) => {
+  const displayElements = () => {
+    if(loading) {
+      return (
+        <>
+          <div className="pdp-left-content">
+            <div className="pdp-image"><img src={product[0].pictureUrl} alt={product[0].title} /></div>
+          </div>
+          <div className="pdp-right-content">
+            <div className="pdp-product-name">{product[0].title}</div>
+            <div className="pdp-sku">SKU: {product[0].sku}</div>
+            <div className="pdp-price">${product[0].price}</div>
+            <div className="pdp-installments">{product[0].installments}</div>
+            <div className="pdp-description-content">
+              <p className="pdp-description-title">Descripción</p>
+              <p className="pdp-description">{product[0].description}</p>
+            </div>
+            <div className="pdp-buy"><button className="pdp-buy-button" >AGREGAR AL CARRITO</button></div>
+          </div>
+        </>
+      )
+    }
+    else {
+      return <h4>Cargando...</h4>
+    }
+  }
+  return displayElements()
+}
+ 
+export default ItemDetail;
